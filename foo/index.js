c6049af528d4c8a9393b1fcadcd890cb7c075a5c
Joona Heikkilä <joona.o.heikkila@gmail.com>
2018-10-27 19:05:57 +0300

    Remove data

--- foo/index.js
+++
@@ -1,8 +0,0 @@
-const foo = require("../data/foo.json");
-
-console.log("Foo:");
-console.log(foo.foo);
-console.log("Bars:");
-console.log(foo.bars);
-console.log("Foobars:");
-console.log(foo.foobars);
