0d9e0ade081d42cfc5e83b52539553eb140d0363
Joona Heikkilä <joona.o.heikkila@gmail.com>
2018-10-27 21:03:00 +0300

    Add NhlStatistics1 project base

---
+++ NhlStatistics1/src/main/java/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import ohtuesimerkki.Player;
+import ohtuesimerkki.Statistics;
+
+public class Main {
+    public static void main(String[] args) {
+        Statistics stats = new Statistics();
+          
+        System.out.println("Philadelphia Flyers");
+        for (Player player : stats.team("PHI") ) {
+            System.out.println( player );
+        }
+        
+        System.out.println("");
+        
+        System.out.println("Top scorers");
+        for (Player player : stats.topScorers(10) ) {
+            System.out.println( player );
+        }        
+    }
+}
